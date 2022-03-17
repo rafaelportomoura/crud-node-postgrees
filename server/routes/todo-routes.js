bb5d5dd2855e4b7372114aeabda44556e8ced93e
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2022-03-17 18:13:20 -0300

    🐛 Fix pg version error

--- server/routes/todo-routes.js
+++ server/routes/todo-routes.js
@@ -14,148 +14,132 @@ var database = require('../config/database.js');
 var conString = database.conString;
 var results = [];
 
-
 module.exports = {
-
-	/*================================================================
+  /*================================================================
 	CREATE - $http post
 	=================================================================*/
-	//create todo and send back all todos after creation
-	createTodo : function(req, res) {
+  //create todo and send back all todos after creation
+  createTodo: async function (req, res) {
+    results = [];
 
-		results = [];
+    //Data to be saved to the DB - taken from $http request packet
+    var data = {
+      text: req.body.text,
+      done: false,
+    };
 
-		//Data to be saved to the DB - taken from $http request packet
-		var data = {
-			text : req.body.text,
-			done : false
-		};
+    let client;
+    try {
+      client = new pg.Client(conString);
+      await client.connect();
 
-  		// get a pg client from the connection pool
-  		pg.connect(conString, function(err, client, done) {
-   			client.query("INSERT INTO todos(text, done) values($1, $2)", [data.text, data.done]);
+      await client.query('INSERT INTO todos(text, done) values($1, $2)', [data.text, data.done]);
 
-			var query = client.query("SELECT * FROM todos ORDER BY id ASC");
+      let query = await client.query('SELECT * FROM todos ORDER BY id ASC');
 
-			//can stream row results back 1 at a time
-			query.on('row', function(row) {
-		      	results.push(row);
-			});
+      results = query.rows;
+    } catch (error) {
+      console.log('Error: ' + error.message);
+    } finally {
+      if (client) {
+        client.end(); // workaround fix
+      }
+    }
 
-			//fired after last row is emitted
-			query.on('end', function() { 
-				client.end();
-				return res.json(results); // return all todos in JSON format  		
-			});
+    return res.json(results);
+  },
 
-			if(err)
-				console.log(err);
-   		});
-    },
-
-
-	/*================================================================
+  /*================================================================
 	READ - $http get
 	=================================================================*/
-	//Get all todos in the database
-	getTodos : function(req, res) {
+  //Get all todos in the database
+  getTodos: async function (req, res) {
+    results = [];
 
-		results = [];
+    let client;
+    try {
+      client = new pg.Client(conString);
+      await client.connect();
 
-		// get a pg client from the connection pool
-  		pg.connect(conString, function(err, client, done) {
-   
-			var query = client.query("SELECT * FROM todos ORDER BY id ASC");
+      let query = await client.query('SELECT * FROM todos ORDER BY id ASC');
 
-			//can stream row results back 1 at a time
-			query.on('row', function(row) {
-		      	results.push(row);
-			});
+      results = query.rows;
+    } catch (error) {
+      console.log('Error: ' + error.message);
+    } finally {
+      if (client) {
+        client.end(); // workaround fix
+      }
+    }
 
-			//fired after last row is emitted
-			query.on('end', function() { 
-			  client.end();
-			  return res.json(results); // return all todos in JSON format
-			});
+    return res.json(results);
+  },
 
-			//console.log()
-			if(err)
-				console.log(err);
-
-   		});
-	},
-
-
-	/*================================================================
+  /*================================================================
 	UPDATE - $http put
 	=================================================================*/
-	updateTodo : function(req, res) {
+  updateTodo: async function (req, res) {
+    results = [];
 
-		results = [];
+    var id = req.params.todo_id;
 
-  		var id = req.params.todo_id;
+    var data = {
+      text: req.body.text,
+      done: req.body.done,
+    };
 
-		var data = {
-			text : req.body.text,
-			done: req.body.done
-		};
+    console.log('ID= ' + id); //TEST
 
-		console.log("ID= "+id); //TEST
+    let client;
+    try {
+      client = new pg.Client(conString);
+      await client.connect();
 
-		// get a pg client from the connection pool
-  		pg.connect(conString, function(err, client, done) {
+      await client.query('UPDATE todos SET text=($1), done=($2) WHERE id=($3)', [
+        data.text,
+        data.done,
+        id,
+      ]);
 
-   			client.query("UPDATE todos SET text=($1), done=($2) WHERE id=($3)", [data.text, data.done, id]);
-			var query = client.query("SELECT * FROM todos ORDER BY id ASC");
+      let query = await client.query('SELECT * FROM todos ORDER BY id ASC');
 
-			//can stream row results back 1 at a time
-			query.on('row', function(row) {
-		      	results.push(row);
-			});
+      results = query.rows;
+    } catch (error) {
+      console.log('Error: ' + error.message);
+    } finally {
+      if (client) {
+        client.end(); // workaround fix
+      }
+    }
 
-			//fired after last row is emitted
-			query.on('end', function() { 
-			  client.end();
-			  return res.json(results); // return all todos in JSON format
-			});
+    return res.json(results);
+  },
 
-			//console.log()
-			if(err)
-				console.log(err);
-   		});	        
-    },
-
-	/*================================================================
+  /*================================================================
 	DELETE - $http delete
 	=================================================================*/
-	deleteTodo : function(req, res) {
+  deleteTodo: async function (req, res) {
+    results = [];
+    var id = req.params.todo_id;
 
-		results = [];
-		var id = req.params.todo_id;
+    console.log('id= ' + id); //TEST
 
-		console.log("id= "+id); //TEST
+    let client;
+    try {
+      client = new pg.Client(conString);
+      await client.connect();
+      await client.query('DELETE FROM todos WHERE id=($1)', [id]);
+      let query = await client.query('SELECT * FROM todos ORDER BY id ASC');
 
-		// get a pg client from the connection pool
-  		pg.connect(conString, function(err, client, done) {
+      results = query.rows;
+    } catch (error) {
+      console.log('Error: ' + error.message);
+    } finally {
+      if (client) {
+        client.end(); // workaround fix
+      }
+    }
 
-   			client.query("DELETE FROM todos WHERE id=($1)", [id]);
-   
-			var query = client.query("SELECT * FROM todos ORDER by id ASC");
-
-			//can stream row results back 1 at a time
-			query.on('row', function(row) {
-		      	results.push(row);
-			});
-
-			//fired after last row is emitted
-			query.on('end', function() { 
-			  client.end();
-			  return res.json(results); // return all todos in JSON format
-			});
-
-			//console.log()
-			if(err)
-				console.log(err);
-   		});	 
-	}
+    return res.json(results);
+  },
 };
